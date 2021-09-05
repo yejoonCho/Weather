f3fa4030f82fb126c67b90047b510ca06b35220e
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-09-05 16:01:00 +0900

    make screen UI

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class WeatherScreen extends StatefulWidget {
   final dynamic parseWeatherData;
@@ -10,8 +14,9 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  String? cityName;
-  double? temperature;
+  var cityName;
+  var temperature;
+  var date = DateTime.now();
 
   @override
   void initState() {
@@ -20,29 +25,176 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   void updateData(dynamic weatherData) {
-    temperature = weatherData['main']['temp'];
     cityName = weatherData['name'];
+    temperature = weatherData['main']['temp'].toInt();
+  }
 
-    print(temperature);
-    print(cityName);
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat("h:mm a").format(now);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.near_me),
+          onPressed: () {},
+          iconSize: 30,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_searching),
+            onPressed: () {},
+            iconSize: 30,
+          )
+        ],
+      ),
+      body: Container(
+        child: Stack(
           children: [
-            Text('$cityName', style: TextStyle(fontSize: 30)),
-            SizedBox(
-              height: 20,
+            Image.asset(
+              'assets/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
             ),
-            Text('$temperature', style: TextStyle(fontSize: 30))
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 150),
+                            Text(
+                              'Seoul',
+                              style: GoogleFonts.lato(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(
+                                  Duration(minutes: 1),
+                                  builder: (context) {
+                                    print('${getSystemTime()}');
+                                    return Text('${getSystemTime()}',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 16, color: Colors.white));
+                                  },
+                                ),
+                                Text(DateFormat(' - EEEE, ').format(date),
+                                    style: GoogleFonts.lato(
+                                        fontSize: 16, color: Colors.white)),
+                                Text(DateFormat('d MMM, yyy').format(date),
+                                    style: GoogleFonts.lato(
+                                        fontSize: 16, color: Colors.white))
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '18\u2103',
+                              style: GoogleFonts.lato(
+                                  fontSize: 80,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset('svg/climacon-sun.svg'),
+                                SizedBox(width: 10),
+                                Text('clear sky',
+                                    style: GoogleFonts.lato(
+                                        fontSize: 16, color: Colors.white))
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Divider(height: 15, thickness: 2, color: Colors.white30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text('AQI (대기질지수)',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 14, color: Colors.white)),
+                              SizedBox(height: 10),
+                              Image.asset('assets/bad.png',
+                                  width: 37, height: 35),
+                              SizedBox(height: 10),
+                              Text('매우나쁨',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      color: Colors.black87,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text('미세먼지',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 14, color: Colors.white)),
+                              SizedBox(height: 10),
+                              Text('174.75',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 24, color: Colors.white)),
+                              SizedBox(height: 10),
+                              Text('ug/m3',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text('초미세먼지',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 14, color: Colors.white)),
+                              SizedBox(height: 10),
+                              Text('84.03',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 24, color: Colors.white)),
+                              SizedBox(height: 10),
+                              Text('ug/m3',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
-      )),
+      ),
     );
   }
 }
